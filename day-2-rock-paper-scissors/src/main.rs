55ccd53f07f78b7074cee7261327833f09c9b5e8
Gil Emmanuel Bancud <gembancud@gmail.com>
2023-01-25 10:51:59 +0800

    Added Day2.2

--- day-2-rock-paper-scissors/src/main.rs
+++ day-2-rock-paper-scissors/src/main.rs
@@ -1,5 +1,11 @@
 use std::fs;
 
+enum state {
+    lose,
+    win,
+    draw,
+}
+
 fn main() {
     // read from input
     let path = String::from("input.txt");
@@ -7,39 +13,48 @@ fn main() {
     let mut score = 0;
     for line in contents.lines() {
         let a = line.split(" ").collect::<Vec<&str>>();
-        let aa = match a[0] {
+        let enemyhand = match a[0] {
             "A" => 0,
             "B" => 1,
             "C" => 2,
             _ => panic!("Invalid input"),
         };
-        let bb = match a[1] {
-            "X" => {
-                score += 1;
-                0
-            }
+        let neededstate = match a[1] {
+            "X" => state::lose,
             "Y" => {
-                score += 2;
-                1
+                score += 3;
+                state::draw
             }
             "Z" => {
-                score += 3;
-                2
+                score += 6;
+                state::win
             }
             _ => panic!("Invalid input"),
         };
-        match (aa, bb) {
-            (0, 0) => score += 3,
-            (0, 1) => score += 6,
-            (0, 2) => score += 0,
-            (1, 0) => score += 0,
-            (1, 1) => score += 3,
-            (1, 2) => score += 6,
-            (2, 0) => score += 6,
-            (2, 1) => score += 0,
-            (2, 2) => score += 3,
-            _ => panic!("Invalid input"),
+        match (enemyhand, neededstate) {
+            (0, state::lose) => score += 3,
+            (0, state::draw) => score += 1,
+            (0, state::win) => score += 2,
+            (1, state::lose) => score += 1,
+            (1, state::draw) => score += 2,
+            (1, state::win) => score += 3,
+            (2, state::lose) => score += 2,
+            (2, state::draw) => score += 3,
+            (2, state::win) => score += 1,
+            _ => (),
         }
+        // match (aa, bb) {
+        //     (0, 0) => score += 3,
+        //     (0, 1) => score += 6,
+        //     (0, 2) => score += 0,
+        //     (1, 0) => score += 0,
+        //     (1, 1) => score += 3,
+        //     (1, 2) => score += 6,
+        //     (2, 0) => score += 6,
+        //     (2, 1) => score += 0,
+        //     (2, 2) => score += 3,
+        //     _ => panic!("Invalid input"),
+        // }
     }
     println!("{}", score);
 }
